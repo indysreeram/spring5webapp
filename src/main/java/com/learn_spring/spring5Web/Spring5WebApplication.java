dde2800d859564b542505d495797f4463beca68a
Sreeram Srinivasaragavan <indysreeram@gmail.com>
2018-10-30 16:18:10 -0400

    intial commit

---
+++ src/main/java/com/learn_spring/spring5Web/Spring5WebApplication.java
@@ -0,0 +1,12 @@
+package com.learn_spring.spring5Web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5WebApplication.class, args);
+	}
+}
